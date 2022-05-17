880de15bdfb1588c575415ec32047ffcb5264a71
Niels <n.j.hoogendoorn@student.tudelft.nl>
2022-05-17 15:48:21 +0200

    ED formula 1

--- Source Code/EnvironmentalDischarge.py
+++ Source Code/EnvironmentalDischarge.py
@@ -1,1 +1,13 @@
-def F1():
+import math as m
+import pandas as pd
+
+data = [['1-1-10',43.56],['1-1-10',41.56]]
+df = pd.DataFrame(data)
+print(df[:][1])
+
+def f1(data):
+    Mean = df.mean()
+    final = Mean*0.5
+    return()
+
+
--- Source Code/main.py
+++ Source Code/main.py
@@ -1,4 +1,5 @@
-# This is a sample Python script.
+from EnvironmentalDischarge import *
+ # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -7,10 +8,12 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    f1(data)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Niels Hoogendoorn')
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
